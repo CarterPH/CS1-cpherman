ca81192224413e77616e2ed941d972552166594a
Carter Herman <carterpherman@gmail.com>
2023-08-24 11:37:52 -0600

    new hello file

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Carter Herman
+
+Thias is a hello world program!!!
+*/
+
+// this is to print out
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+
+    return 0;
+}
