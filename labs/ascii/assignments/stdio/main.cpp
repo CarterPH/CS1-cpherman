97cc26b6300d015ea407e8e8f331d878a4c9fe97
Carter Herman <carterpherman@gmail.com>
2023-08-28 11:30:52 -0600

    base program

---
+++ labs/ascii/assignments/stdio/main.cpp
@@ -0,0 +1,23 @@
+/*
+Carter Herman
+CSCI 111 Section 02
+11:00 am - 11:50 am M-F
+
+This program prints each step of the game hangman with input from the user of what their name is.
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char name;
+    cout << "Hello! Whats your name? ";
+    
+
+
+
+
+}
