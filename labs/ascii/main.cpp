0367b313c6ed521048592bd02e019e2e0be496bc
Carter Herman <carterpherman@gmail.com>
2023-08-31 11:57:25 -0600

    added new line commands

--- labs/ascii/main.cpp
+++ labs/ascii/main.cpp
@@ -33,11 +33,11 @@ int main()
 
     string line2 = " /  @  @ \\      	*                   ASCII Lab               *  	  (='.'=)\n";
     cout << line2;
-    string line3 = "( >   0  <   )       *              By: Carter Herman      * 	( \" )_( \" )";
+    string line3 = "( >   0  <   )       *              By: Carter Herman      * 	( \" )_( \" )\n";
     cout << line3;
-    string line4 = ">>x<<        	*                     CSCI 111              *";
+    string line4 = ">>x<<        	*                     CSCI 111              *\n";
     cout << line4;
-    string line5 = "   /   O  \\       	***************************** ";
+    string line5 = "   /   O  \\       	***************************** \n";
     cout << line5;
     //FIXME6: use variable to print the second line line of the graphic
     //FIXME7: print the third line of the graphics
