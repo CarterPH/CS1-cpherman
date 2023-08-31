69eb9456385b724f65eec1dede68032badac95aa
Carter Herman <carterpherman@gmail.com>
2023-08-31 12:49:20 -0600

    Final Submission

--- labs/ascii/main.cpp
+++ labs/ascii/main.cpp
@@ -28,22 +28,17 @@ int main()
 
     cout << "Hope you like my Tom & Jerry ASCII art...\n\n";
 
-    string line1 = "  |\\_/|   **********************    (\\_/)\n";
+    string line1 = "  |\\_/|         *****************************              (\\_/)\n";
     cout << line1;
 
-    string line2 = " /  @  @ \\      	*                   ASCII Lab               *  	  (='.'=)\n";
+    string line2 = " / @ @\\      	*             ASCII Lab            *  	  (='.'=)\n";
     cout << line2;
-    string line3 = "( >   0  <   )       *              By: Carter Herman      * 	( \" )_( \" )\n";
+    string line3 = "( > 0  < )       *        By: Carter Herman         *  	 (\" )_(\" )\n";
     cout << line3;
-    string line4 = ">>x<<        	*                     CSCI 111              *\n";
+    string line4 = "  >>x<<      	*                CSCI 111          *\n";
     cout << line4;
-    string line5 = "   /   O  \\       	***************************** \n";
+    string line5 = "  / O \\     	***************************** \n";
     cout << line5;
-    //FIXME6: use variable to print the second line line of the graphic
-    //FIXME7: print the third line of the graphics
-    //FIXME8: use variable to print the fourth line
-    //FIXME9: use variable to print the fifth line
-    //Note: You can add more lines or print more ASCII arts of your choice if you'd like...
 
     cout << "\nGood bye... hit enter to exit the program: " << '\n';
     cin.get();
