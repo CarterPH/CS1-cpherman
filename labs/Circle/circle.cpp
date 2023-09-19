a9f05dea15dbc3e2ea840a543cbe2a70ee549e2f
Carter Herman <carterpherman@gmail.com>
2023-09-19 10:47:01 -0600

    added comments

---
+++ labs/Circle/circle.cpp
@@ -0,0 +1,16 @@
+/*
+ Circle Lab
+ By: Carter Herman
+ CSCI 111
+ Date: 9/19/2023
+
+ This program prompts the user to enter the radius of a circle.
+ It then calculates and displays it area and circumference.
+
+ Algorithm steps:
+ 1. Prompt user to enter radius of a circle and store the radius into a variable
+ 2. Calculate area using the formula pi x radius x radius and store the value into a variable
+ 3. Calculate circumference using the formula 2 x pi x radius and store the value into a variable
+ 4. Output the calculated values for area and circumference
+
+*/
