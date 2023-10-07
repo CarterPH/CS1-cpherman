2b02f18a10a9973eec128d49a282eeb6d49be612
Carter Herman <carterpherman@gmail.com>
2023-10-07 02:49:16 -0600

    added sum

---
+++ assignments/functions/functions.cpp
@@ -0,0 +1,39 @@
+/* 
+Carter Herman
+CSCI 111
+10/07/2023
+
+This is a program that is a basic calculator using functions,
+
+*/
+
+
+
+
+#include <iostream>
+#include <cstdio>
+#include <cassert>
+#include <cmath>
+using namespace std;
+
+void main() {
+    float in1;
+    float in2;
+
+    cout << "Please input your first number." << endl;
+    cin >> in1;
+
+    cout << "Please input your second number. " << endl;
+    cin >> in2;
+
+
+
+}
+
+void sum(float in2, float in1) {
+    float sum = in1 + in2;
+    cout << "The sum of your numbers is " << sum << endl;
+    
+    return main();
+}
+
