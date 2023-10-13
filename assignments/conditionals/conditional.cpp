57b279ea1c1a63b34ae3dfd17edbfcfb506411d4
Carter Herman <carterpherman@gmail.com>
2023-10-13 11:44:13 -0600

    finished full base cofe

--- assignments/conditionals/conditional.cpp
+++ assignments/conditionals/conditional.cpp
@@ -100,5 +100,105 @@ double findSmaller(const double &n1, const double &n2, const double &n3, const d
 
 
 void test() {
-    double answer
+    double answer = findSum(10, 12.5, 1.1, 0.01, 22);
+    double expected = 46.61;
+    assert(answer == expected); 
+    assert(findSum(-5, 10.5, -11, -41, 11) == -35.5);
+
+    double answer2 = findProduct(2, 1, 11, 23, 12);
+    double expected2 = 6072;
+    assert(answer2 == expected2);
+    assert(findProduct(5, 3, -1, -14, 11) == 2310);
+
+    double answer3 = findLarger(2, 1, -10, 3, 19);
+    double expected3 = 19;
+    assert(answer3 == expected3);
+    assert(findLarger(5, 3, -4, 1, 10) == 10);
+
+    double answer4 = findSmaller(2, 1, -15, 7, 2.2);
+    double expected4 = -15;
+    assert(answer4 == expected4);
+    assert(findSmaller(5, 3, -3.14, 10, 11) == -3.14);
+
+
+    double avg;
+    findAverage(2, 1, 11, -1, -9, avg);
+    double expected7 = 0.8;
+    assert(avg == expected7);
+    findAverage(6, 2, 11, 101, 6, avg);
+    double expected8 = 121.2;
+    assert(avg == expected8);
+    printf("%s\n", "all test cases passed..."); 
+}
+
+bool program() {
+    int option = 1;
+    double num1=0, num2=0, num3=0, num4=0, num5=0;
+    printMenu();
+
+    do {
+        if (cin >> option && option >= 1 && option <= 7) {
+            break;
+        }
+        else {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid option, please enter a value betweem 1 and 6" << endl;
+        }
+    } while(true);
+
+    switch(option) {
+        case 1:
+        {
+            getFiveNumbers(num1, num2, num3, num4, num5);
+
+            double sum = findSum(num1, num2, num3, num4, num5);
+            printf("%.2f + %.2f + %.2f + %.2f + %.2f = %.2f\n", num1, num2, num3, num4, num5, sum);
+            break;
+
+        }
+        case 2:
+        {
+            getFiveNumbers(num1, num2, num3, num4, num5);
+
+            double product = findProduct(num1, num2, num3, num4, num5);
+            printf("%.2f * %.2f * %.2f * %.2f * %.2f = %.2f\n", num1, num2, num3, num4, num5, product);
+            break;
+        }
+        case 3:
+        {
+            getFiveNumbers(num1, num2, num3, num4, num5);
+
+            double avg;
+            findAverage(num1, num2, num3, num4, num5, avg);
+            printf("%.2f + %.2f + %.2f + %.2f + %.2f / 5 = %.2f\n", num1, num2, num3, num4, num5, avg);
+            break;
+        }
+        case 4:
+        {
+            getFiveNumbers(num1, num2, num3, num4, num5);
+
+            double max = findLarger(num1, num2, num3, num4, num5);
+            printf("The largest between all five numbers is %.2f\n", max);
+
+        }
+        case 5:
+        {
+            getFiveNumbers(num1, num2, num3, num4, num5);
+
+            double min = findSmaller(num1, num2, num3, num4, num5);
+            printf("The smallest between all five numbers is %.2f\n", min);
+            
+        }
+        case 6:
+        {
+            getFiveNumbers(num1, num2, num3, num4, num5);
+
+            
+        }
+        case 7:
+        default:
+            return false;
+    }
+    return true;
 }
