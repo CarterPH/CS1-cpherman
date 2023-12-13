53a3f4b8000b93924222e3f84c449a8e330f5c13
Carter Herman <carterpherman@gmail.com>
2023-12-13 09:40:55 -0700

    finished final

--- finalproject/main.cpp
+++ finalproject/main.cpp
@@ -23,7 +23,7 @@ const int MAX_TRIES = 6;
 void readData(const int, ifstream &, string[]);
 void writeFile(const int[], ofstream &, const string[], int);
 int letterAmt(const string [], char [], const int);
-void ascii(int, const string[], int);
+void ascii(int);
 int guess(const string[], char[], int &maxTries, int wordIndex);
 
 int main() {
@@ -48,6 +48,7 @@ int main() {
     cin.get();
 
     int tryAmt = 0;
+    int gamesWon = 0;
     char option;
 
     int scoreList[MAXL];
@@ -56,8 +57,8 @@ int main() {
     readData(MAXL, inputFile, words);
 
     do {
-        system("cls");
-        int wordIndex = rand() % MAXL;
+        system("clear");
+        int wordIndex = rand() %  12;
         int maxTries = MAX_TRIES;
         char blank[MAXS] = {}; 
         int gameScore = guess(words, blank, maxTries, wordIndex);
@@ -65,6 +66,12 @@ int main() {
         scoreList[tryAmt] = gameScore;
         wordList[tryAmt] = words[wordIndex];
         tryAmt++;
+        
+        if (gameScore == words[wordIndex].length()) {
+            gamesWon++;
+        }
+
+        cout << "Games won: " << gamesWon << endl;
 
         cout << "Would you like to play again? (y/n)" << endl;
         cin >> option;
@@ -96,25 +103,30 @@ void writeFile(const int scoreList[], ofstream &outputFile, const string wordLis
 }
 
 int letterAmt(const string words[], char blank[], int MAXL) {
-    int randomWord = rand() % MAXL;
+    int randomWord = rand() % 12;
 
     for (std::string::size_type i = 0; i < words[randomWord].length(); i++) {
         blank[i] = '_';
+        cout << blank[i] << " ";
     }
-
+    cout << endl;
     return randomWord;
 }
 
 int guess(const string words[], char blank[], int &maxTries, int wordIndex) {
     int score = 0;
     int guessAmt = 0;
-    char guessedLetters[26] = {};  
-    const int MAX_WORD_LENGTH = words[0].length();
+    char guessedLetters[26] = {};
+    const int MAX_WORD_LENGTH = words[wordIndex].length();
 
-    while (guessAmt < maxTries) {
+    while (guessAmt < MAX_TRIES && maxTries > 0) {
         cout << "Your word: ";
         for (int i = 0; i < MAX_WORD_LENGTH; i++) {
-            cout << blank[i] << " ";
+            if (guessedLetters[tolower(words[wordIndex][i]) - 'a'] || words[wordIndex][i] == ' ') {
+                cout << words[wordIndex][i] << " ";
+            } else {
+                cout << "_ ";
+            }
         }
         cout << endl;
 
@@ -130,15 +142,13 @@ int guess(const string words[], char blank[], int &maxTries, int wordIndex) {
         char guess;
         cin >> guess;
 
-        
         if (!(('a' <= guess && guess <= 'z') || ('A' <= guess && guess <= 'Z'))) {
             cout << "Invalid input. Please enter a letter." << endl;
             continue;
         }
 
-        guess = tolower(guess);  
+        guess = tolower(guess);
 
-        
         if (guessedLetters[guess - 'a']) {
             cout << "You already guessed '" << guess << "'. Try another letter." << endl;
             continue;
@@ -149,63 +159,71 @@ int guess(const string words[], char blank[], int &maxTries, int wordIndex) {
         bool correctGuess = false;
         for (int i = 0; i < MAX_WORD_LENGTH; i++) {
             if (guess == tolower(words[wordIndex][i])) {
-                blank[i] = words[wordIndex][i];
-                score++;
+                if (blank[i] == '_') {
+                    blank[i] = words[wordIndex][i];
+                    
+                }
                 correctGuess = true;
+                score = score + 1;
             }
         }
 
-        if (!correctGuess) {
-            maxTries--;
-            ascii(MAX_TRIES - maxTries, words, wordIndex);
+        if (correctGuess) {
+        cout << "Your word: ";
+        for (int i = 0; i < MAX_WORD_LENGTH; i++) {
+            cout << blank[i] << " ";
         }
-
+        cout << endl;
+        } else {
+            maxTries--;
+            ascii(MAX_TRIES - maxTries);
+            guessAmt++;
+        }
         if (score == MAX_WORD_LENGTH) {
             cout << "Congratulations! You guessed the word: " << words[wordIndex] << endl;
             break;
         }
-
-        guessAmt++;
     }
+
+    
+    cout << "Your word: ";
+    for (int i = 0; i < MAX_WORD_LENGTH; i++) {
+        if (guessedLetters[tolower(words[wordIndex][i]) - 'a'] || words[wordIndex][i] == ' ') {
+            cout << words[wordIndex][i] << " ";
+        } else {
+            cout << "_ ";
+        }
+    }
+    cout << endl;
 
     if (score != MAX_WORD_LENGTH) {
         cout << "Sorry, you ran out of guesses. The word was: " << words[wordIndex] << endl;
+        ascii(MAX_TRIES - maxTries);
     }
 
     return score;
 }
-void ascii(int wrong, const string words[], int wordIndex){
-    
 
-    if (wrong == 1) {
-        cout << "\t\t  O   " << endl;
-    }
-    if (wrong == 2) {
-        cout << "\t\t  O   " << endl
-             << "\t\t  |   " << endl;
-    }
-    if (wrong == 3) {
-        cout << "\t\t  O   " << endl
-             << "\t\t /|   " << endl;
-    }
-    if (wrong == 4) {
-        cout << "\t\t  O   " << endl
-             << "\t\t /|\\  " << endl;
-    }
-    if (wrong == 5) {
-        cout << "\t\t  O   " << endl
-             << "\t\t /|\\  " << endl
-             << "\t\t  |   " << endl;
-    }
-    if (wrong == 6) {
-        cout << "\t\t  O   " << endl
-             << "\t\t /|\\  " << endl
-             << "\t\t / \\  " << endl;
 
-        cout << "You lose!" << endl;
-        cout << "The word was " << words[wordIndex] << endl;
 
-        
+
+void ascii(int wrong) {
+    cout << "\t\t  _______" << endl;
+    cout << "\t\t  |     |" << endl;
+
+    if (wrong >= 1) {
+        cout << "\t\t  |     O" << endl;
     }
-    
+    if (wrong >= 2) {
+        cout << "\t\t  |     |" << endl;
+    }
+    if (wrong >= 3) {
+        cout << "\t\t  |    /|\\ " << endl;
+    }
+    if (wrong >= 4) {
+        cout << "\t\t  |    / \\" << endl;
+    }
+
+    cout << "\t\t__|__" << endl;
 }
+
